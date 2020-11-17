70065f13d73c9d2eba152293bdbcfb353f3b8fd4
wesorth <70714434+wesorth@users.noreply.github.com>
2020-11-17 17:35:34 -0500

    added a count function
    gives a 5s delay after 30 trigger pulls

--- Senior_Laser_rifle.ino
+++ Senior_Laser_rifle.ino
@@ -1,6 +1,6 @@
 int laserPin = 10;
 int triggerPin = 9;
-
+int count =0;
 void setup ()
 {
   pinMode(laserPin, OUTPUT);
@@ -13,5 +13,10 @@ void loop () {
     delay(100);
     digitalWrite(laserPin, LOW);
     delay(100);
+    count++;
+  }
+  if(count ==30){
+    delay(5000); ///5s delay
+    count =0;
   }
 }
