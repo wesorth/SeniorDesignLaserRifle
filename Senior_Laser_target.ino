cb7492d65eba64a63a2518b11c329cf902e6394b
wesorth <70714434+wesorth@users.noreply.github.com>
2020-12-02 14:15:59 -0500

    Add files via upload
    start of frequency read

--- Senior_Laser_target.ino
+++ Senior_Laser_target.ino
@@ -2,7 +2,11 @@
 int receivePin = 10;
 int outPin = 9;
 int val=0;
-
+int high=0;
+int timeTotal = 0;
+int count = 0;
+int T = 0;
+int frequency = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(receivePin, INPUT);
@@ -14,5 +18,19 @@ void loop() {
   val = digitalRead(receivePin);
   if(receivePin==HIGH){
     digitalWrite(outPin, HIGH); // turn on LED signal
+    // start timer
+    high =1;
+  }
+  if(receivePin == LOW && high==1){
+    high = 0;
+    //end timer
+    //timetotal+= timer
+    count++;
+  }
+  if(count ==20){
+    T = timeTotal / count; // average period time
+    frequency = 1/T;// frequency calculation
+    count = 0;//reset count and timeTotal to 0
+    timeTotal = 0;  
   }
 }
