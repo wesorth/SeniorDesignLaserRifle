17e6f1cf1c58814f0c108eda8b2b5ab800de3d8b
wesorth <70714434+wesorth@users.noreply.github.com>
2020-12-02 13:49:59 -0500

    Add files via upload
    now should turn on LED, doesnt measure frequency yet

--- Senior_Laser_target.ino
+++ Senior_Laser_target.ino
@@ -1,15 +1,18 @@
+
 int receivePin = 10;
+int outPin = 9;
 int val=0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(receivePin, INPUT);
+  pinMode(outPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   val = digitalRead(receivePin);
   if(receivePin==HIGH){
-    digitalWrite(
+    digitalWrite(outPin, HIGH); // turn on LED signal
   }
 }
