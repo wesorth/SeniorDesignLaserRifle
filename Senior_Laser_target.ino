8de3260368840e2a886fe6ad5674f99defc8b930
wesorth <70714434+wesorth@users.noreply.github.com>
2020-12-02 20:37:54 -0500

    Add files via upload
    added frequency measuring

--- Senior_Laser_target.ino
+++ Senior_Laser_target.ino
@@ -6,11 +6,12 @@ int high=0;
 int timeTotal = 0;
 int count = 0;
 int T = 0;
-int frequency = 0;
+int frequency = 0, start=0, stopTime =0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(receivePin, INPUT);
   pinMode(outPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -19,18 +20,23 @@ void loop() {
   if(receivePin==HIGH){
     digitalWrite(outPin, HIGH); // turn on LED signal
     // start timer
+    start = micros();
     high =1;
   }
   if(receivePin == LOW && high==1){
     high = 0;
     //end timer
-    //timetotal+= timer
+    stopTime = micros();
+    timeTotal = timeTotal + (stopTime - start);
     count++;
   }
-  if(count ==20){
+  if(count==20){
     T = timeTotal / count; // average period time
     frequency = 1/T;// frequency calculation
     count = 0;//reset count and timeTotal to 0
     timeTotal = 0;  
+    Serial.print("frequency is ");
+    Serial.print(frequency);
+    Serial.prinln();
   }
 }
