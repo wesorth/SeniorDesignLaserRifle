c56ddb36d78f0ab4e602600f690e7dd40d5a7712
wesorth <70714434+wesorth@users.noreply.github.com>
2020-12-07 15:31:39 -0500

    Add files via upload
    changed input from digital to analog, calculates frequency successfully.

--- Senior_Laser_target.ino
+++ Senior_Laser_target.ino
@@ -1,42 +1,44 @@
-
-int receivePin = 10;
-int outPin = 9;
-int val=0;
+int sensorPin = A0;
+int sensorValue = 0;
 int high=0;
-int timeTotal = 0;
-int count = 0;
-int T = 0;
-int frequency = 0, start=0, stopTime =0;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(receivePin, INPUT);
-  pinMode(outPin, OUTPUT);
-  Serial.begin(9600);
-}
+long timeTotal = 0;
+float count = 0;
+float T = 0;
+float frequency = 0;
+unsigned long start=0, stopTime =0;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  val = digitalRead(receivePin);
-  if(receivePin==HIGH){
-    digitalWrite(outPin, HIGH); // turn on LED signal
-    // start timer
-    start = micros();
-    high =1;
-  }
-  if(receivePin == LOW && high==1){
-    high = 0;
-    //end timer
-    stopTime = micros();
-    timeTotal = timeTotal + (stopTime - start);
-    count++;
-  }
-  if(count==20){
-    T = timeTotal / count; // average period time
-    frequency = 1/T;// frequency calculation
-    count = 0;//reset count and timeTotal to 0
-    timeTotal = 0;  
-    Serial.print("frequency is ");
-    Serial.print(frequency);
-    Serial.prinln();
+void setup(void) {
+   Serial.begin(9600);
+   pinMode(sensorPin, INPUT); 
+}
+ 
+void loop(void) {
+   sensorValue = analogRead(sensorPin);
+   //Serial.println(sensorValue);
+    if(sensorValue>30 && high ==0){
+      //digitalWrite(outPin, HIGH); // turn on LED signal
+      // start timer
+      start = millis();
+      //Serial.println(start);
+      high =1;
+      }
+    if(sensorValue<30 && high==1){
+      high = 0;
+      //end timer
+      stopTime = millis();
+      //Serial.println(stopTime);
+      timeTotal = timeTotal + (stopTime - start);
+      count++;
+    }
+    if(count==2){
+      T = (timeTotal / count)/1000; // average period time
+      //Serial.print("period");
+      //Serial.println(T);
+      frequency = 1/T;// frequency calculation
+      count = 0;//reset count and timeTotal to 0
+      timeTotal = 0;  
+      Serial.print("frequency is ");
+      Serial.println(frequency);
+      Serial.print(" ");
   }
 }
