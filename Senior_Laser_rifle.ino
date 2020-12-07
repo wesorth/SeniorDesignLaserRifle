aeb99ffba348d4fa4a4b474c8e639dd3de6e5877
wesorth <70714434+wesorth@users.noreply.github.com>
2020-12-07 15:32:30 -0500

    added a buttonlock

--- Senior_Laser_rifle.ino
+++ Senior_Laser_rifle.ino
@@ -1,6 +1,7 @@
 int laserPin = 10;
 int triggerPin = 9;
 int count =0;
+int btnlock = 0;
 void setup ()
 {
   pinMode(laserPin, OUTPUT);
@@ -8,14 +9,19 @@ void setup ()
 }
 
 void loop () {
-  if(triggerPin==HIGH){
-    for(int temp = 0; temp <250; temp++){ //500Hz
+  //digitalRead(triggerPin);
+  if(btnlock == 1  && digitalRead(triggerPin) ==LOW){
+    btnlock = 0;
+  }
+  if(btnlock == 0 && digitalRead(triggerPin)==HIGH){
+    for(int temp = 0; temp <10; temp++){ //500Hz
       digitalWrite(laserPin, HIGH);
-      delay(1);
+      delay(1000);
       digitalWrite(laserPin, LOW);
-      delay(1);
+      delay(1000);
     }
     count++;
+    btnlock =1;
   }
   if(count ==30){
     delay(5000); ///5s delay
